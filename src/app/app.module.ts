11352852849fdae77d304d6a5d8e9eba7937d3c3
Dillon <astrophizz@gmail.com>
2019-01-23 14:13:01 -0600

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './home/home.component';
+
+@NgModule({
+  declarations: [AppComponent, HomeComponent],
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    AppRoutingModule,
+  ],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
